c68161d25829abf42611e78f1dda74ea72531f6a
RahimaKhanam <rahima.khanam45@gmail.com>
2022-01-17 18:54:58 +0530

    Front-end

---
+++ client/src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from "@apollo/client";
+
+// components
+import BookList from "./components/BookList";
+import AddBook from "./components/AddBook";
+
+// ApolloClient Setup
+const client = new ApolloClient({
+  uri: "http://localhost:8000/graphql",
+  cache: new InMemoryCache(),
+});
+
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <div id="main">
+          <h1>Reading List</h1>
+          <BookList />
+          <AddBook/>
+        </div>
+      </ApolloProvider>
+    );
+  }
+}
+
+export default App;
--- server/app.js
+++ server/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
-const schema = require('./schema/schema')
-const mongoose = require('mongoose')
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+const cors = require('cors');
  
 const app = express();
+
+// allow cross-origin requests
+app.use(cors());
 
 mongoose.connect(
     "mongodb+srv://rahima:rahima@cluster0.aocxo.mongodb.net/graphql-project")
